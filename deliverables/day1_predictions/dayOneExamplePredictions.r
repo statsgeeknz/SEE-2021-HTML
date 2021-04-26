219f4fc15b0f38a48ba6e90c90499ba8de7c0f06
Statsgeek <statsgeeknz@googlemail.com>
2021-04-26 20:27:34 +0100

    Update dayOneExamplePredictions.r
    Replaced example file to be mhtml hosted on github. Use new 123 day scraper. Added additional day's data to JSON.

--- deliverables/day1_predictions/dayOneExamplePredictions.r
+++ deliverables/day1_predictions/dayOneExamplePredictions.r
@@ -4,18 +4,19 @@
 
 
   library(jsonlite)  
-  source("scrape_oneTwoDay.r")
+  source("scrape_oneTwoThreeDay.r")
   source("tools.r")
   source("predict_fault_1_day_forecast.R")
 
 
   # scrape an example HTML file
-  exampleForecast <-  weatherScraperOneTwoDay("north_2015_10_03_05_11_41.html") 
+  exampleForecast <-  weatherScraperOneTwoThreeDay("https://statsgeeknz.github.io/") 
   
   # create a list object with the two days separated. 
   # Using names inferred from JSON file sent from Bellrock
   exampleForecastList <- list("1_day_forecast" = exampleForecast %>% filter(day == 1),
-                              "2_day_forecast" = exampleForecast %>% filter(day == 2))
+                              "2_day_forecast" = exampleForecast %>% filter(day == 2),
+                              "3_day_forecast" = exampleForecast %>% filter(day == 3))
   
   # write to JSON format
   exampleForecastJSON <- toJSON(exampleForecastList)
