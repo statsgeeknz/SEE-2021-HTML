00e559ced8bd94e85c4f7c0ae610dfe36ffb2ff5
Statsgeek <statsgeeknz@googlemail.com>
2021-07-11 13:10:55 +0100

    Update SSE_2021_checking modifications.R
    added new feeds for testing against

--- SSE_2021_checking modifications.R
+++ SSE_2021_checking modifications.R
@@ -1,6 +1,10 @@
-# run scrapers on supplied (m)HTML
+# run scrapers on supplied HTML
 
+# 3 random feeds for each
 weatherScraperOneTwoThreeDay("http://feed.meteogroup.co.uk/SSE/north_2021_07_05_06_08_33.html")
-
+weatherScraperOneTwoThreeDay("http://feed.meteogroup.co.uk/SSE/north_2021_07_10_05_39_13.html")
+weatherScraperOneTwoThreeDay("http://feed.meteogroup.co.uk/SSE/north_2021_07_06_15_20_03.html")
 
 weatherScraperFourFiveDay("http://feed.meteogroup.co.uk/SSE/north_2021_07_05_06_08_33.html")
+weatherScraperFourFiveDay("http://feed.meteogroup.co.uk/SSE/north_2021_07_10_05_39_13.html")
+weatherScraperFourFiveDay("http://feed.meteogroup.co.uk/SSE/north_2021_07_06_15_20_03.html")
